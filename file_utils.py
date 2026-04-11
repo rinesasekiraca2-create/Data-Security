8495bc078d97cc489da4486c22c9b162efd27718
diona_coder1 <diona_coder1@example.com>
2026-04-11 21:53:43 +0200

    Add file reading and writing functions

--- file_utils.py
+++ file_utils.py
@@ -1,1 +1,20 @@
+def read_file(filename):
 
+    try:
+
+        with open(filename, 'r', encoding='utf-8') as f:
+
+            return f.read().lower()
+
+    except FileNotFoundError:
+
+        print(f"File {filename} nuk u gjet.")
+
+        return ""
+
+
+def save_output(filename, text):
+
+    with open(filename, 'w', encoding='utf-8') as f:
+
+        f.write(text)
