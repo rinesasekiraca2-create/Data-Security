809f2c6b1f1de4c0c1ae27c62621604cf9a108f2
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-04-14 16:14:55 +0200

    attack.py
    Analiza e frekuencave te shkronjave

--- attack.py
+++ attack.py
@@ -3,3 +3,13 @@ from frequency import calculate_frequency
 import string
 
 ALPHABET = string.ascii_lowercase
+
+def chi_squared(observed, expected):
+    score = 0
+    for c in ALPHABET:
+        o = observed.get(c, 0)
+        e = expected.get(c, 0)
+        if e > 0:
+            score += ((o - e) ** 2) / e
+    return score
+
