48becce6691b35e8e41d20d28a0192f2580e557b
diona_coder1 <diona_coder1@example.com>
2026-04-14 20:07:19 +0200

    Add calculate_frequency function

--- frequency.py
+++ frequency.py
@@ -3,3 +3,13 @@ from collections import Counter
 import string
 
 ALPHABET = string.ascii_lowercase
+def calculate_frequency(text):
+    filtered = [c for c in text if c in ALPHABET]
+    total = len(filtered)
+
+    freq = Counter(filtered)
+
+    if total == 0:
+        return {c: 0 for c in ALPHABET}
+
+    return {c: freq[c] / total for c in ALPHABET}
