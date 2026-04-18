4fc8d80dc2b68faf3191ce16686ea4527c8b0a13
djellzaramaja <djellza.ramaja@student.uni-pr.edu>
2026-04-19 01:14:21 +0200

    Outputi i tekstit te dekriptuar

--- main.py
+++ main.py
@@ -15,29 +15,29 @@ def main():
     cipher = read_file("cipher.txt")
 
 
-
     if not reference or not cipher:
 
         print("Gabim me file-at!")
 
         return
 
-  ref_freq = calculate_frequency(reference)
-
-
+    ref_freq = calculate_frequency(reference)
 
     print_frequency(ref_freq)
 
-
-
     shift, decrypted = break_caesar(cipher, ref_freq)
 
-
-
     print("\n========================")
-
     print(f"SHIFT I GJETUR: {shift}")
-
     print("========================\n")
 
+    print("TEKSTI I DEKRIPTUAR:\n")
+    print(decrypted)
 
+    save_output("output.txt", decrypted)
+
+    print("\n U ruajt ne output.txt")
+
+
+if __name__ == "__main__":
+    main()
